285b49482b13ad9db3c53a9bd2734c7b95edf957
dofkamnb-oss <dofkamnb@gmail.com>
2026-08-08 09:51:48 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLTD/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "حمدان المنصوري | زايد المزروعي",
